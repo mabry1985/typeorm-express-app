8bc0154696bb122dded57040d1b86b37bda4e6d4
Josh Mabry <mabry1985@gmail.com>
2020-11-18 16:50:25 -0800

    add connection to orm and create endpoint function callbacks

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,42 @@
 import * as express from "express";
 import { Request, Response } from "express";
+import { createConnection } from "typeorm";
+import {User} from "./entity/User"; 
 
-const app = express();
-app.use(express.json());
+createConnection().then(connection => {
+  const userRepository = connection.getRepository(User);
 
-app.get("/users", function (req: Request, res: Response) {
-
-});
-
-app.get("/users/:id", function(req: Request, res: Response) {
-
-});
-
-app.post("/users", function(req: Request, res: Response) {
+  const app = express();
+  app.use(express.json());
   
-});
+  app.get("/users", async function (req: Request, res: Response) {
+    const users = await userRepository.find();
+    res.json(users);
+  });
+  
+  app.get("/users/:id", async function(req: Request, res: Response) {
+    const results = await userRepository.findOne(req.params.id);
+    return res.send(results);
+  });
+  
+  app.post("/users", async function(req: Request, res: Response) {
+    const user = await userRepository.create(req.body);
+    const results = await userRepository.save(user);
+    return res.send(results);
+  });
+  
+  app.put("/users/:id", async function(req: Request, res: Response) {
+    const user = await userRepository.findOne(req.params.id);
+    userRepository.merge(user, req.body);
+    const results = await userRepository.save(user);
+    return res.send(results);
+  });
+  
+  app.delete("/users/:id", async function(req: Request, res: Response) {
+    const results = await userRepository.delete(req.params.id);
+    return res.send(results);
+  });
+  
+  app.listen(3000);
 
-app.put("/users/:id", function(req: Request, res: Response) {
-
-});
-
-app.delete("/users/:id", function(req: Request, res: Response) {
-
-});
-
-app.listen(3000);
+})
